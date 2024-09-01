1c3136c9b43aec5e7bd2933de4db8f6afd98d407
BharatDasa <dasabharat90@gmail.com>
2024-09-01 05:45:41 -0700

    Update loging.java

--- src/main/java/Basic/loging.java
+++ src/main/java/Basic/loging.java
@@ -1,5 +1,13 @@
 package Basic;
 
 public class loging {
+  
+	//from Dev-1 + Branch b11 no conflict
+	
+	public void logingwithcredetial(String username, String password)
+	{
+		System.out.println("username = +" username);
+		System.out.println("Password = +" password);
+	}
 
 }
